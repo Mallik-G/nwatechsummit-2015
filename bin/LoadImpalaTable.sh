a6fa9d41d6ec50edb0027bf6cade76a1a1f976ae
superhadooper <richard@svds.com>
2017-05-21 18:06:06 -0500

    updated for Spark 2

--- bin/LoadImpalaTable.sh
+++ bin/LoadImpalaTable.sh
@@ -1,8 +1,9 @@
-~/nwatechsummit-2015/spark-1.6.0-bin-hadoop2.6/bin/spark-submit \
---repositories https://repository.cloudera.com/artifactory/repo/ \
---packages "org.apache.kudu:kudu-spark_2.10:1.3.0,org.apache.spark:spark-streaming_2.10:1.6.0,"\
-"org.apache.spark:spark-streaming-kafka_2.10:1.6.0,org.apache.spark:spark-sql_2.10:1.6.0" \
+#!/bin/bash
+/home/u/nwatechsummit-2015/spark-2.1.1-bin-hadoop2.7/bin/spark-submit \
+--repositories https://repository.cloudera.com/artifactory/cloudera-repos/ \
+--packages "org.apache.kudu:kudu-spark_2.10:1.3.0,org.apache.spark:spark-streaming_2.1.0:2.1.0,"\
+"org.apache.spark:spark-streaming-kafka_2.10:2.0.0-cloudera1-SNAPSHOT,org.apache.spark:spark-sql_2.1.0:2.1.0" \
 --class com.svds.kudumeetup.KuduMeetupParquetLoad \
-../target/scala-2.10/kudu-meetup_2.10-1.0.jar \
+/home/u/nwatechsummit-2015/target/scala-2.10/kudu-meetup_2.10-1.0.jar \
 /user/ubuntu/meetup_parquet \
 file:///disk1/meetupstream
